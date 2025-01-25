56361494f561bafd6e9be156b2bc9331161acb38
Ath <athenais.klp@gmail.com>
2025-01-25 14:14:35 +0100

    alert modified

--- script.js
+++ script.js
@@ -80,12 +80,13 @@ window.onscroll = function(ev) {
 verifyNewPost();
 async function verifyNewPost() {
   let actualID = await fetchingData('maxitem.json');
+  const alertDiv = document.getElementById('alert');
   
   setInterval(async () => {
     try {
       const maxID = await fetchingData('maxitem.json');
       if (actualID !== maxID) {
-        alert("❗ ALERT : New content available, check it out!");
+        alertDiv.style.display = 'block';
         actualID = maxID;
       }
     } catch (error) {
