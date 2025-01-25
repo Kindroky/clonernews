800298ccfd2afbe2a8f17546f1a8f2e8c7ceefe9
cnuttens <clement.nutt@gmail.com>
2025-01-25 12:24:49 +0100

    Append new posts

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //CONSTANTS
-const VERBOSE = 2;
+const VERBOSE = 1;
 const BASE_URL = "https://hacker-news.firebaseio.com/v0/";
 const THEME = "newstories.json";
 const POSTS_CONTAINER = document.getElementById("posts-container");
@@ -24,7 +24,7 @@ async function fetchingData(data) {
 }
 //fetching post data
 async function fetchingPosts(id) {
-  const post = await fetchingData(`${BASE_URL}item/${id}.json`);
+  const post = await fetchingData(`item/${id}.json`);
   if (VERBOSE >= 1) {
     console.log(post);
   }
@@ -51,8 +51,19 @@ async function add_10_Posts() {
   const tmp_posts = await POSTS;
   const slice = tmp_posts.slice(actual_len, actual_len + 10);
 
+  //For each new post, add it to the container
   for (const id of slice) {
     const post = await fetchingPosts(id);
+    if (VERBOSE >= 2) console.log("Post ID:", post.id);
+    const newPostDiv = document.createElement("div");
+    newPostDiv.classList.add("post");
+    newPostDiv.innerHTML = `
+    <h2>${post.title}</h2>
+    <p>${post.score} points</p>
+    <p>By <strong>${post.by}</strong></p>
+    <p>${new Date(post.time * 1000).toLocaleString()}</p>
+    <p>URL : <a href="${post.url}">link</a></p> `;
+    POSTS_CONTAINER.appendChild(newPostDiv);
   }
 }
 
